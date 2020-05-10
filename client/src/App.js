bb67dc4068c19de7596f3a6badb32271254bd270
Kerwin Hy <kerwin.hy@gmail.com>
2020-05-09 19:21:20 -0700

    1. removed App.css because styling will be done through public folder 2. added imports to App.js for store provider, main page, and router

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Main from "./pages/main";
+import { StoreProvider } from "./utils/globalState";
 
 function App() {
   return (
